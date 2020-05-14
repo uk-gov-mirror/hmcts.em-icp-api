1383d19f8ec0cc8461f40ca3bf4c17c52bfb664c
meganaslattery <43603312+meganaslattery@users.noreply.github.com>
2020-05-14 16:53:39 +0100

    Remove fe files (#1)
    * Remove webpack
    
    * Remove FE files
    
    * Change port
    
    * Linting
    
    * Delete irrelevant tests
    
    * Delete more tests

--- src/main/app.ts
+++ src/main/app.ts
@@ -7,9 +7,7 @@ import express from 'express';
 import { Helmet } from './modules/helmet';
 import * as path from 'path';
 import { RouterFinder } from './router/routerFinder';
-import favicon from 'serve-favicon';
 import { HTTPError } from 'HttpError';
-import { Nunjucks } from './modules/nunjucks';
 const { setupDev } = require('./development');
 
 const env = process.env.NODE_ENV || 'development';
@@ -23,15 +21,12 @@ app.use(Express.accessLogger());
 
 const logger = Logger.getLogger('app');
 
-new Nunjucks(developmentMode).enableFor(app);
 // secure the application by adding various HTTP headers to its responses
 new Helmet(config.get('security')).enableFor(app);
 
-app.use(favicon(path.join(__dirname, '/public/assets/images/favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.setHeader(
     'Cache-Control',
@@ -44,7 +39,7 @@ setupDev(app,developmentMode);
 // returning "not found" page for requests with paths not resolved by the router
 app.use((req, res) => {
   res.status(404);
-  res.render('not-found');
+  res.send('not-found');
 });
 
 // error handler
@@ -55,5 +50,5 @@ app.use((err: HTTPError, req: express.Request, res: express.Response) => {
   res.locals.message = err.message;
   res.locals.error = env === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
