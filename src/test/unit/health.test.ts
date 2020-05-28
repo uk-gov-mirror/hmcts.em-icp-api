3eef7053b974268c1f10a5b4830ce0f0abb72623
tomxelliott <tomelliott@Toms-MBP.home>
2020-05-28 10:31:36 +0100

    Add test case

---
+++ src/test/unit/health.test.ts
@@ -0,0 +1,17 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { app } from '../../main/app';
+
+chai.use(chaiHttp);
+
+describe('/GET health', () => {
+  it('it should return (200) OK', (done) => {
+    chai.request(app)
+      .get('/health')
+      .end((err, res) => {
+        chai.expect(res.body).to.be.an('object');
+        chai.expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
