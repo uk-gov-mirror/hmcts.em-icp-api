7874f564eda95aad5767d33ffc98882905535d35
Damon Green <damon.green@btinternet.com>
2020-06-23 12:51:40 +0100

    add some api GW conf different method

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -121,9 +121,8 @@ resource "azurerm_key_vault_secret" "local_redis_password" {
 module "em-icp-api" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
 
-  api_mgmt_name = core-api-mgmt-aat
-  api_mgmt_rg   = core-infra-aat
-
+  api_mgmt_name = "core-api-mgmt-aat"
+  api_mgmt_rg   = "core-infra-aat"
   name = "em-icp-api"
 }
 
