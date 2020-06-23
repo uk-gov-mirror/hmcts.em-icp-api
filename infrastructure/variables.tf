29f06524412613a43ed11d36efcf037cfad5cd19
damongreen123 <damon.green@btinternet.com>
2020-06-23 14:10:22 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -44,10 +44,6 @@ variable "api_mgmt_rg" {
   description = "Resource group that api management is in, e.g. core-infra-demo"
 }
 
-variable "api_mgmt_name" {
-  description = "Name of the api management, e.g. core-infra-demo"
-}
-
 variable "name" {
   description = "Name of the product"
 }
