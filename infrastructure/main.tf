a22bcab9796651391cbfcc2d6fcd046632464a83
damongreen123 <damon.green@btinternet.com>
2020-06-24 10:22:54 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -130,20 +130,20 @@ module "em-icp-api" {
 module "api" {
   source        = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
   name          = "${var.product}-icp-api"
-  api_mgmt_rg   = "core-infra-aat"
-  api_mgmt_name = "core-api-mgmt-aat"
+  api_mgmt_rg   = "core-infra-${var.env}"
+  api_mgmt_name = "core-api-mgmt-${var.env}"
   display_name  = "${var.product}-icp"
   revision      = "1"
   product_id    = "${module.em-icp-api.product_id}"
   path          = "${local.api_base_path}"
-  service_url   = "http://em-icp-aat.service.core-compute-aat.internal"
+  service_url   = "http://em-icp-${var.env}.service.core-compute-${var.env}.internal"
   swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/cnp-plum-recipes-service.json"
 }
 
 module "policy" {
   source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
-  api_mgmt_name          = "core-api-mgmt-aat"
-  api_mgmt_rg            = "core-infra-aat"
+  api_mgmt_name          = "core-api-mgmt-${var.env}"
+  api_mgmt_rg            = "core-infra-${var.env}"
   api_name               = "${module.api.name}"
   api_policy_xml_content = "${local.api_policy}"
 }
