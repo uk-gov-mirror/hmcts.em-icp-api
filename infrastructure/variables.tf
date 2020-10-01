feab168352385344c243994a7899cad10c50f37d
benouaer <tech@benouaer.com>
2020-10-01 14:20:50 +0100

    Revert "revert azurermv2 upgrade"
    This reverts commit edce96cc

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,49 +1,36 @@
-variable "product" {
-  type = "string"
-}
+variable "product" {}
 
-variable "component" {
-  type = "string"
-}
+variable "component" {}
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
-variable "env" {
-  type = "string"
-}
+variable "env" {}
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "subscription" {
-  type = "string"
-}
+variable "subscription" {}
 
 variable "managed_identity_object_id" {
   default = ""
 }
 
 variable "appinsights_location" {
-  type        = "string"
   default     = "West Europe"
   description = "Location for Application Insights"
 }
 
 # thumbprint of the SSL certificate for API gateway tests
 variable api_gateway_test_certificate_thumbprint {
-  type = "string"
-
   # keeping this empty by default, so that no thumbprint will match
   default = ""
 }
