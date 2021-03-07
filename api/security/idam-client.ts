9de783b02b44ac8b1109cd4ad9b629f67bdb0ca2
Taleb Benouaer <tech@benouaer.com>
2021-03-07 19:46:28 +0000

    check jwk

--- api/security/idam-client.ts
+++ api/security/idam-client.ts
@@ -46,7 +46,7 @@ export class IdamClient {
   private async getJwks(algorithm: string) {
     this.logger.info("this the algorithm", algorithm);
     const response = await this.http.get("/o/jwks");
-    this.logger.info("this the jwKey", response.data);
+    this.logger.info("this the jwKey", response.data[2]);
     return response.data.keys.find((key) => key.alg === algorithm);
   }
 }
