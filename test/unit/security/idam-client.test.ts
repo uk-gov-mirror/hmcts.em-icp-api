f18c6d91aeec059b291356512fc3c587b28222b5
Taleb Benouaer <tech@benouaer.com>
2021-03-07 19:16:23 +0000

    fix failing test

--- test/unit/security/idam-client.test.ts
+++ test/unit/security/idam-client.test.ts
@@ -38,7 +38,6 @@ describe("IdamClient", () => {
       .catch((err) => {
         expect(err.message).to.contain("Invalid token specified:");
         expect(idamClient.logger.error.called).to.be.true;
-        expect(idamClient.logger.info.called).to.be.true;
       });
   });
 });
