d63d3801cf8181a0f87470ecbd21ac7d6a23a2fd
hsjhita <harpreet.jhita@justice.gov.uk>
2021-03-16 16:03:00 +0000

    TA-569 Checkstyle fixes
    TA-569 Checkstyle fixes

--- test/pact/pact-tests/settings/provider.mock.ts
+++ test/pact/pact-tests/settings/provider.mock.ts
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const { Pact } = require('@pact-foundation/pact');
+const path = require("path");
+const { Pact } = require("@pact-foundation/pact");
 
 export interface PactTestSetupConfig {
   provider: string;
@@ -15,11 +15,11 @@ export class PactTestSetup {
 
   constructor(config: PactTestSetupConfig) {
     this.provider = new Pact({
-      consumer: 'em_icpApi',
-      dir: path.resolve(process.cwd(), 'test/pact/pacts'),
-      log: path.resolve(process.cwd(), 'test/pact/logs', 'mockserver-integration.log'),
-      logLevel: 'info',
-      pactfileWriteMode: 'merge',
+      consumer: "em_icpApi",
+      dir: path.resolve(process.cwd(), "test/pact/pacts"),
+      log: path.resolve(process.cwd(), "test/pact/logs", "mockserver-integration.log"),
+      logLevel: "info",
+      pactfileWriteMode: "merge",
       port: this.port,
       provider: config.provider,
       spec: 2,
--- test/pact/publish/publish.ts
+++ test/pact/publish/publish.ts
@@ -1,31 +1,30 @@
-import pact from '@pact-foundation/pact-node';
-import * as git from 'git-rev-sync';
-import * as path from 'path';
+import pact from "@pact-foundation/pact-node";
+import * as git from "git-rev-sync";
+import * as path from "path";
 
-const config = require('config');
+const config = require("config");
 const publish = async (): Promise<void> => {
   try {
 
-    const pactBroker = config.get('pact.brokerUrl') ?
-    config.get('pact.brokerUrl') : 'http://localhost:80';
+    const pactBroker = config.get("pact.brokerUrl") ?
+      config.get("pact.brokerUrl") : "http://localhost:80";
 
-    const pactTag = config.get('pact.branchName') ?
-    config.get('pact.branchName') : 'Dev';
+    const pactTag = config.get("pact.branchName") ?
+      config.get("pact.branchName") : "Dev";
 
-    const consumerVersion = config.get('pact.consumerVersion') !== '' ?
-      // @ts-ignore
-      config.get('pact.consumerVersion') : git.short();
+    const consumerVersion = config.get("pact.consumerVersion") !== "" ?
+      config.get("pact.consumerVersion") : git.short();
 
-    const certPath = path.resolve(__dirname, '../cer/ca-bundle.crt');
+    const certPath = path.resolve(__dirname, "../cer/ca-bundle.crt");
     process.env.SSL_CERT_FILE = certPath;
 
     const opts = {
       consumerVersion,
       pactBroker,
-      pactBrokerPassword: '',
-      pactBrokerUsername: '',
+      pactBrokerPassword: "",
+      pactBrokerUsername: "",
       pactFilesOrDirs: [
-        path.resolve(__dirname, '../pacts/'),
+        path.resolve(__dirname, "../pacts/"),
       ],
       publishVerificationResult: true,
       tags: [pactTag],
@@ -33,13 +32,13 @@ const publish = async (): Promise<void> => {
 
     await pact.publishPacts(opts);
 
-    console.log('Pact contract publishing complete!');
-    console.log('');
+    console.log("Pact contract publishing complete!");
+    console.log("");
     console.log(`Head over to ${pactBroker}`);
-    console.log('to see your published contracts.');
+    console.log("to see your published contracts.");
 
   } catch (e) {
-    console.log('Pact contract publishing failed: ', e);
+    console.log("Pact contract publishing failed: ", e);
   }
 };
 
