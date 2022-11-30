a3f14be676f4dca26e937298500824c0f1454b3e
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-11-30 11:26:09 +0000

    Updated terraform version details and added in web pubsub

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,30 +1,34 @@
-variable "product" {}
+variable product {
+  default = "em"
+}
 
-variable "component" {}
+variable component {
+  default = "icp"
+}
 
-variable "location" {
+variable location {
   default = "UK South"
 }
 
-variable "env" {}
+variable env {}
 
-variable "common_tags" {
+variable common_tags {
   type = map(string)
 }
 
-variable "tenant_id" {}
+variable tenant_id {}
 
-variable "jenkins_AAD_objectId" {
+variable jenkins_AAD_objectId {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "subscription" {}
+variable subscription {}
 
-variable "managed_identity_object_id" {
+variable managed_identity_object_id {
   default = ""
 }
 
-variable "appinsights_location" {
+variable appinsights_location {
   default     = "West Europe"
   description = "Location for Application Insights"
 }
