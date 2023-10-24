4b13a5864cbac1bc0b9c4911f8ae964e5cd11d1c
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-10-24 14:50:01 +0100

    comment out pubsub

--- app.ts
+++ app.ts
@@ -25,18 +25,18 @@ const env = process.env.NODE_ENV || "development";
 propertiesVolume.addTo(config);
 
 const APP_INSIGHTS_KEY = config.secrets ? config.secrets["em-icp"]["AppInsightsInstrumentationKey"] : undefined;
-const primaryConnectionstring = config.secrets ? config.secrets["em-icp"]["em-icp-web-pubsub-primary-connection-string"] : undefined;
+// const primaryConnectionstring = config.secrets ? config.secrets["em-icp"]["em-icp-web-pubsub-primary-connection-string"] : undefined;
 
 const logger = Logger.getLogger("app");
 
 export const app = express();
 app.locals.ENV = env;
 
-// const limiter = rateLimit({
-//   windowMs: config.rateLimit.time,
-//   max: config.rateLimit.max,
-// });
-//
+const limiter = rateLimit({
+  windowMs: config.rateLimit.time,
+  max: config.rateLimit.max,
+});
+
 // const webPubSubOptions = new EmWebPubEventHandlerOptions(primaryConnectionstring);
 // const handler = new WebPubSubEventHandler("hub", {
 //   path: "/eventhandler",
@@ -45,58 +45,58 @@ app.locals.ENV = env;
 //   onConnected: webPubSubOptions.onConnected,
 //   onDisconnected: webPubSubOptions.onDisconnected,
 // });
-//
+
 // app.use(handler.getMiddleware());
-// app.use(limiter);
-// app.use(Express.accessLogger());
-//
-// if (APP_INSIGHTS_KEY) {
-//   appInsights.setup(APP_INSIGHTS_KEY)
-//     .setAutoCollectConsole(true, true)
-//     .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-//     .setSendLiveMetrics(true)
-//     .start();
-//   appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "em-icp";
-// }
-//
-// app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-//
-// app.use(noCache());
-// app.use(helmet());
-// app.use(helmet.xssFilter({ setOnOldIE: true }));
-//
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Cache-Control",
-//     "no-cache, max-age=0, must-revalidate, no-store",
-//   );
-//   next();
-// });
-//
-// app.use("/", RouterFinder.findAll(path.join(__dirname, "api/routes")));
-//
-// app.use((req, res) => {
-//   res.status(404);
-//   res.send("not-found");
-// });
-//
-// app.use((err: HttpError, req: express.Request, res: express.Response) => {
-//   logger.error(`${err.stack || err}`);
-//
-//   res.locals.message = err.message;
-//   res.locals.error = env === "development" ? err : {};
-//   res.status(err.status || 500);
-//   res.send("error");
-// });
-//
-// if (config.app.useCSRFProtection === "true") {
-//   app.use(csrf(), (req, res, next) => {
-//     res.locals.csrfToken = req.csrfToken();
-//     next();
-//   });
-// }
+app.use(limiter);
+app.use(Express.accessLogger());
+
+if (APP_INSIGHTS_KEY) {
+  appInsights.setup(APP_INSIGHTS_KEY)
+    .setAutoCollectConsole(true, true)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+    .setSendLiveMetrics(true)
+    .start();
+  appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "em-icp";
+}
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(noCache());
+app.use(helmet());
+app.use(helmet.xssFilter({ setOnOldIE: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Cache-Control",
+    "no-cache, max-age=0, must-revalidate, no-store",
+  );
+  next();
+});
+
+app.use("/", RouterFinder.findAll(path.join(__dirname, "api/routes")));
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("not-found");
+});
+
+app.use((err: HttpError, req: express.Request, res: express.Response) => {
+  logger.error(`${err.stack || err}`);
+
+  res.locals.message = err.message;
+  res.locals.error = env === "development" ? err : {};
+  res.status(err.status || 500);
+  res.send("error");
+});
+
+if (config.app.useCSRFProtection === "true") {
+  app.use(csrf(), (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  });
+}
 
 app.use("/health", healthcheck);
