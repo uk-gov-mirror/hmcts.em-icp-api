f29232ca4e3fa47243e791cbbedf4adebed1d362
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-10-26 09:55:40 +0100

    remove local key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,17 +35,17 @@ data "azurerm_user_assigned_identity" "em-shared-identity" {
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
-module "local_key_vault" {
-  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                     = local.app_full_name
-  env                         = var.env
-  tenant_id                   = var.tenant_id
-  object_id                   = var.jenkins_AAD_objectId
-  resource_group_name         = azurerm_resource_group.rg.name
-  product_group_object_id     = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
-  common_tags                 = var.common_tags
-  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.em-shared-identity.principal_id}", "${var.managed_identity_object_id}"]
-}
+#module "local_key_vault" {
+#  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+#  product                     = local.app_full_name
+#  env                         = var.env
+#  tenant_id                   = var.tenant_id
+#  object_id                   = var.jenkins_AAD_objectId
+#  resource_group_name         = azurerm_resource_group.rg.name
+#  product_group_object_id     = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
+#  common_tags                 = var.common_tags
+#  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.em-shared-identity.principal_id}", "${var.managed_identity_object_id}"]
+#}
 
 #data "azurerm_key_vault" "s2s_vault" {
 #  name                = "s2s-${local.local_env}"
