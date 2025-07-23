aff945976ac34d810fb00041e7e0de42ab33b676
Josh <josh.glasgow@hmcts.net>
2025-07-23 13:42:41 +0100

    Update to use exui group id for assigning roles

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -196,18 +196,9 @@ resource "azurerm_key_vault_secret" "em_icp_web_pubsub_primary_connection_string
   key_vault_id = module.local_key_vault.key_vault_id
 }
 
-variable "user_ids" {
-    type = list(string)
-    default = [
-      "3689a22e-1785-4944-bd9e-113355bfb070"
-    ]
-    description = "List of user IDs to grant the Web PubSub Service Owner role to."
-}
-
 resource "azurerm_role_assignment" "web_pubsub_service_owner" {
-  for_each = local.local_env != "prod" ? toset(var.user_ids) : []
-
+  count                = local.local_env != "prod" ? 1 : 0
   scope                = azurerm_web_pubsub.ped_web_pubsub.id
   role_definition_name = "Web PubSub Service Owner"
-  principal_id         = each.value
+  principal_id         = "7a7c6518-2381-408b-940c-7b9bd0256d9a"
 }
